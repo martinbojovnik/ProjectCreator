28634a55c92a6585608ac5cb1ae81bde4bc5f63d
martin <martin.perout123@post.cz>
2024-01-24 20:55:22 +0100

    gathering informations

--- main.py
+++ main.py
@@ -4,3 +4,27 @@ if not os.path.exists("Profiles"):
     os.mkdir("Profiles")
 
 allFiles = os.listdir("Profiles")
+if not allFiles == []:
+    for num, i in enumerate(allFiles):
+        if i.endswith('.prof'):
+            print(f"{num+1}: {i.replace('.prof', '')}")
+else:
+    print("No Profiles Found")
+    exit()
+
+while True:
+    print("")
+    inp = input("> ")
+    try:
+        if int(inp) <= len(allFiles):
+            break
+    except:
+        pass
+
+ProfileToCreate = int(inp)
+
+while True:
+    print("")
+    name = input("NameOfTheProject> ")
+    if not name == "":
+        break
